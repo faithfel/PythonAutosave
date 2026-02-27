6cf0f29e8065cdeae684ffb5c2a0690a633b68d3
Faith <felominofaith@gmail.com>
2026-02-27 22:53:06 +0800

    ctk

--- ui.py
+++ ui.py
@@ -1,14 +1,20 @@
 import customtkinter as CTK
+import keyboard
+import time
+
 
 app = CTK.CTk()  
 app.geometry("400x240")
-app.title("Installation Test")
+app.title("PyAutosave")
 
-def button_event():
-    print("button pressed")
+def COUNTDOWN():
+    TIMER_INT = int(input("Time before each Autosave:"))
+    time.sleep(TIMER_INT)
+    keyboard.press_and_release('ctrl + s')
+    print("Save Successfully!!")
 
-button = CTK.CTkButton(app, text="CTkButton", command=button_event)
 
-
+button = CTK.CTkButton(master=app, text="START", command=COUNTDOWN)
+button.pack(padx=15, pady=10)
 
 app.mainloop()
