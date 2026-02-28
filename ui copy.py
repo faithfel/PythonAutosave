0ce87d0283ca0da44379b4bfb9dce55679839456
Faith <felominofaith@gmail.com>
2026-02-28 10:42:00 +0800

    added themes

--- ui copy.py
+++ ui copy.py
@@ -7,25 +7,27 @@ import time
 app = CTK.CTk()  
 app.geometry("400x240")
 app.title("PyAutosave")
+CTK.set_appearance_mode("Dark")
+CTK.set_default_color_theme("themes/orange.json")
 
 TITLE_HEADER = CTK.CTkLabel(master=app, text="PYTHON AUTOSAVE", font= ("bold",20))
 TITLE_HEADER.pack(pady=20)
 
 
 def START_TIMER():
-    TIMER_VAL = entry.get()
+    TIMER_VAL = ENTRY.get()
     print(TIMER_VAL)
     time.sleep(int(TIMER_VAL))
     keyboard.press_and_release('ctrl + s')
     print("Save Successfully!!")
 
 
-entry = CTK.CTkEntry(app, placeholder_text="CTkEntry")
-entry.pack(pady=20, padx=20)
-entry.get
+ENTRY = CTK.CTkEntry(app, placeholder_text="CTkEntry")
+ENTRY.pack(pady=20, padx=20)
+ENTRY.get
 
 
-button = CTK.CTkButton(master=app, text="START", command=START_TIMER)
-button.pack(padx=15, pady=10)
+BUTTON = CTK.CTkButton(master=app, text="START", command=START_TIMER)
+BUTTON.pack(padx=15, pady=10)
 
 app.mainloop()
